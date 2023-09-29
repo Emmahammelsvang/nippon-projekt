4e0a7342625a1cdbaf31031c423b78392ee82fa4
Emmahammelsvang <hammelsvang123@gmail.com>
2023-09-29 12:37:18 +0200

    blandet

---
+++ script.js
@@ -0,0 +1,19 @@
+const burgermenu = document.getElementById("burgermenu");
+const hiddenMenu = document.getElementById("hiddenMenu");
+
+burgermenu.onclick = toggleMenu;
+let menuVisible = false;
+
+function toggleMenu(){
+    if(menuVisible == false){
+        console.log("Vis menuen!");
+        menuVisible = true;
+        hiddenMenu.style.opacity = "1";
+        // vis hiddenMenu
+    }else{
+        console.log("Skjul menuen");
+        menuVisible = false;
+        hiddenMenu.style.opacity = "0";
+        // skjul hiddenMenu
+    }
+}
